eca224cc2fd9355d5d06ca76103338c06bac4240
Sunjida Akter <sanjidabintesayid7@gmail.com>
2022-09-26 23:43:44 +0600

    new route file created

--- services/tours.service.js
+++ services/tours.service.js
@@ -1,41 +1,41 @@
-// const Tours = require("../models/Stock");
+const Tours = require("../models/Tours");
 
-// exports.getAllToursService = async (queries) => {
-//     const tours = await Tours
-//         .find({})
-//         .skip(queries.skip)
-//         .limit(queries.limit)
-//         .select(queries.fields)
-//         .sort(queries.sortBy)
-//     return tours;
-// }
-// exports.createTourService = async (data) => {
-//     const tours = await Tours.create(data)
-//     return tours;
-// }
-// exports.updateOneTourByIdService = async (id, data) => {
-//     const tours = await Tours.updateOne(
-//         { _id: id },
-//         { $set: data },
-//         {
-//             runValidators: true,
-//         }
-//     );
-//     return tours;
-// }
-// exports.getOneTourByIdService = async (id) => {
-//     const incViewCount = await Tours.updateOne(
-//         { _id: id },
-//         { $inc: { views: 1 } }
-//     );
-//     const tours = await Tours.findById(id);
-//     return [tours, incViewCount];
-// }
-// exports.getCheapestTourService = async () => {
-//     const tours = await Tours.find({}).sort("price").limit(3);
-//     return tours;
-// }
-// exports.getTrendingTourService = async () => {
-//     const tours = await Tours.find({}).sort("-views").limit(3);
-//     return tours;
-// }
+exports.getAllToursService = async (queries) => {
+    const tours = await Tours
+        .find({})
+        .skip(queries.skip)
+        .limit(queries.limit)
+        .select(queries.fields)
+        .sort(queries.sortBy)
+    return tours;
+}
+exports.createTourService = async (data) => {
+    const tours = await Tours.create(data)
+    return tours;
+}
+exports.updateOneTourByIdService = async (id, data) => {
+    const tours = await Tours.updateOne(
+        { _id: id },
+        { $set: data },
+        {
+            runValidators: true,
+        }
+    );
+    return tours;
+}
+exports.getOneTourByIdService = async (id) => {
+    const incViewCount = await Tours.updateOne(
+        { _id: id },
+        { $inc: { views: 1 } }
+    );
+    const tours = await Tours.findById(id);
+    return [tours, incViewCount];
+}
+exports.getCheapestTourService = async () => {
+    const tours = await Tours.find({}).sort("price").limit(3);
+    return tours;
+}
+exports.getTrendingTourService = async () => {
+    const tours = await Tours.find({}).sort("-views").limit(3);
+    return tours;
+}
